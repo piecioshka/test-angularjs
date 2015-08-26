e55538648f051f65da3163e8411674fe89d9c37a
Piotr Kowalski <piecioshka@gmail.com>
2015-08-26 11:17:13 +0200

    Arrow Function issue.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,24 @@
-(function (root) {
+(function () {
     'use strict';
 
     var mod = angular.module('misc', []);
 
+    mod.controller('ArrowFunctionController', function ($scope) {
+        console.log('ArrowFunctionController was created');
+        $scope.title = 'Function';
+
+        var self = this;
+
+        console.debug(' - normal context', this);
+
+        // Mozilla Firefox support Arrow Function natively.
+        setTimeout(() => {
+            console.debug(' - delay context', this);
+            $scope.title = 'Arrow Function?';
+            // $scope.$digest();
+            console.debug('is context equals?', angular.equals(self, this));
+        }, 1000);
+    });
+
     angular.bootstrap(document, ['misc']);
-}(this));
+}());
